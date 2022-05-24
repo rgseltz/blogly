aedbf8fd755e3509bf98cd2c88fdd9b3b5d33f32
ryan <ryanjgecelter@gmail.com>
2022-05-24 08:53:44 +0300

    added a new class to models.py called Post that instantiates user posts and updated seed file to fill blogly_db with starter posts

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
@@ -26,3 +29,20 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User first_name={self.first_name}, last_name={self.last_name}, image={self.image_url}>"
+
+
+class Post(db.Model):
+    """Posts Model"""
+
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.utcnow)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='posts')
+
+    def __repr__(self):
+        return f'<Post id={self.id} title={self.title} content={self.content} created_at={self.created_at} user_id={self.user_id}'
--- seed.py
+++ seed.py
@@ -1,18 +1,40 @@
-from models import User, db
+from models import User, db, Post
 from app import app
 
 db.drop_all()
 db.create_all()
 
 User.query.delete()
+Post.query.delete()
 
 ry = User(first_name="Ryan", last_name="Seltz")
 shishi = User(first_name="Ashira", last_name="Seltz")
 ari = User(first_name="Aryeh", last_name="Seltz")
 ash = User(first_name="Asher", last_name="Gordon")
+larry = User(first_name="Larry", last_name="David")
+jerry = User(first_name="Jerry", last_name="Seinfeld")
+forest = User(first_name="Forest", last_name="Whittaker")
 
 db.session.add(ry)
 db.session.add(shishi)
 db.session.add(ari)
 db.session.add(ash)
+db.session.add(larry)
+db.session.add(jerry)
+db.session.add(forest)
+db.session.commit()
+
+p1 = Post(title="Life",
+          content="laurem ipsum knows all the answers to all the questions", user_id=1)
+p2 = Post(title="breath", content="to breathe is to live is to laugh", user_id=2)
+p3 = Post(title="Towards a Better Tomorrow",
+          content="Often, we have to face the reality of our situations", user_id=1)
+p4 = Post(title="Goals",
+          content="Only through hard work, clear vision, and perseverence, do we succeed", user_id=3)
+
+db.session.add(p1)
+db.session.add(p2)
+db.session.add(p3)
+db.session.add(p4)
+
 db.session.commit()
